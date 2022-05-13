9ed43bdc14027052e6c6c251cb25f7d211861499
Cool-ShowTTV <22648256+Cool-showTTV@users.noreply.github.com>
2022-05-13 15:54:02 -0400

    Some more clean up and add some comments

--- download.py
+++ download.py
@@ -2,45 +2,53 @@ import requests, os
 
 class Info:
     def getPageCount(name):
+        '''Get the page count of the given comic name'''
         url = f'https://yiffer.xyz/api/comics/{name}'
         response = requests.get(url).json()
         return response['numberOfPages']
 
     def getTags(name):
+        '''Get the tags of the given comic name'''
         url = f'https://yiffer.xyz/api/comics/{name}'
         response = requests.get(url).json()
         return response['keywords']
 
     def getRating(name):
+        '''Get the rating of the given comic name'''
         url = f'https://yiffer.xyz/api/comics/{name}'
         response = requests.get(url).json()
         return response['userRating']
 
     def getComicOrder(name):
+        '''Get the next and previous comics of the given comic name'''
         url = f'https://yiffer.xyz/api/comics/{name}'
         response = requests.get(url).json()
         return {"nextComic": response['nextComic'], "previousComic": response['previousComic']}
 
-def getPage(name, page):
-    page = format(page, "03d")
-    url = f'https://static.yiffer.xyz/comics/{name}/{page}.jpg'
-    response = requests.get(url)
-    return response.content
+class Comic:
+    def getPage(name, page):
+        '''Get the page data of the given comic name and page number'''
+        page = format(page, "03d")
+        url = f'https://static.yiffer.xyz/comics/{name}/{page}.jpg'
+        response = requests.get(url)
+        return response.content
 
-def downloadPage(name, page):
-    print(f'Downloading page {page} of "{name}"', end='\r')
-    if not os.path.exists(comicName):
-        os.makedirs(comicName)
-    with open(f'{name}/{format(page, "03d")}.jpg', 'wb') as f:
-        f.write(getPage(name, page))
+    def downloadPage(name, page):
+        '''Download the given comic name and page number'''
+        print(f'Downloading page {page} of "{name}"', end='\r')
+        if not os.path.exists(comicName):
+            os.makedirs(comicName)
+        with open(f'{name}/{format(page, "03d")}.jpg', 'wb') as f:
+            f.write(Comic.getPage(name, page))
 
-def downloadComic(name):
-    pageCount = Info.getPageCount(name)
-    print(f'Downloading "{name}" with {pageCount} pages')
-    for page in range(1, pageCount + 1):
-        downloadPage(name, page)
-    print(f'Finished downloading {pageCount} pages of "{name}"')
+    def downloadComic(name):
+        '''Download the entire given comic'''
+        pageCount = Info.getPageCount(name)
+        print(f'Downloading "{name}" with {pageCount} pages')
+        for page in range(1, pageCount + 1):
+            Comic.downloadPage(name, page)
+        print(f'Finished downloading {pageCount} pages of "{name}"')
 
 if __name__ == '__main__':
     comicName = input('Comic name: ')
-    downloadComic(comicName)
+    Comic.downloadComic(comicName)
