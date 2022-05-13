dc6b8b6f5ace81f49776f82afc2852cddfc2514a
Cool-ShowTTV <22648256+Cool-showTTV@users.noreply.github.com>
2022-05-13 15:51:16 -0400

    Clean up the code a bit

--- download.py
+++ download.py
@@ -1,9 +1,25 @@
 import requests, os
 
-def getPageCount(name):
-    url = f'https://yiffer.xyz/api/comics/{name}'
-    response = requests.get(url).json()
-    return response['numberOfPages']
+class Info:
+    def getPageCount(name):
+        url = f'https://yiffer.xyz/api/comics/{name}'
+        response = requests.get(url).json()
+        return response['numberOfPages']
+
+    def getTags(name):
+        url = f'https://yiffer.xyz/api/comics/{name}'
+        response = requests.get(url).json()
+        return response['keywords']
+
+    def getRating(name):
+        url = f'https://yiffer.xyz/api/comics/{name}'
+        response = requests.get(url).json()
+        return response['userRating']
+
+    def getComicOrder(name):
+        url = f'https://yiffer.xyz/api/comics/{name}'
+        response = requests.get(url).json()
+        return {"nextComic": response['nextComic'], "previousComic": response['previousComic']}
 
 def getPage(name, page):
     page = format(page, "03d")
@@ -12,18 +28,19 @@ def getPage(name, page):
     return response.content
 
 def downloadPage(name, page):
-    print(f'Downloading page {page} of {name}', end='\r')
+    print(f'Downloading page {page} of "{name}"', end='\r')
+    if not os.path.exists(comicName):
+        os.makedirs(comicName)
     with open(f'{name}/{format(page, "03d")}.jpg', 'wb') as f:
         f.write(getPage(name, page))
 
 def downloadComic(name):
-    pageCount = getPageCount(name)
+    pageCount = Info.getPageCount(name)
+    print(f'Downloading "{name}" with {pageCount} pages')
     for page in range(1, pageCount + 1):
         downloadPage(name, page)
-    print(f'Finished downloading {pageCount} pages of {name}')
+    print(f'Finished downloading {pageCount} pages of "{name}"')
 
 if __name__ == '__main__':
     comicName = input('Comic name: ')
-    if not os.path.exists(comicName):
-        os.makedirs(comicName)
     downloadComic(comicName)
